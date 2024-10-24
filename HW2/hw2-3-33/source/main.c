169fa03fa6671f6921845e0529cf0afde49312dd
zz17321123 <zz17321123@gmail.com>
2024-10-24 21:08:16 +0800

    hw2-3-33 added user input function

--- HW2/hw2-3-33/source/main.c
+++ HW2/hw2-3-33/source/main.c
@@ -4,15 +4,25 @@
 
 int main()
 {
-	for (int i = 0; i < 3; i++) // 橫列(Row)
+	int row;
+	int column;
+
+	printf("Enter rectangle length: ");
+	scanf("%d", &row);
+
+	printf("Enter rectangle breadth: ");
+	scanf("%d", &column);
+
+
+	for (int i = 0; i < row; i++) // 橫列(Row)
 	{
-		for (int j = 0; j < 12; j++) // 直行(Column)
+		for (int j = 0; j < column; j++) // 直行(Column)
 		{
-			if ((i == 0) || (i == 2)) // 判斷是否為 第0列 或 第2列 時，填滿 + 號
+			if ((i == 0) || (i == row-1)) // 判斷是否為 第0列 或 第row-1(最後1列)列 時，填滿 + 號
 				printf("+");
 			else // 反之 第1列 時
 			{
-				if ((j == 0) || (j == 11)) // 判斷是否為 第0行 或 第11行，填充 + 號，其餘行填出 空格 號
+				if ((j == 0) || (j == column -1)) // 判斷是否為 第0行 或 第column-1(最後1行)行，填充 + 號，其餘行填出 空格 號
 					printf("+");
 				else
 					printf(" ");
